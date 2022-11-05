6f92aa010ffe486102c6a823c0540b1690a0dc16
diellesyliqi <63744153+diellesyliqi@users.noreply.github.com>
2022-11-05 21:13:25 +0100

    evaluation plots

--- 02_Code/Functions/Evaluation.R
+++ 02_Code/Functions/Evaluation.R
@@ -1,93 +1,84 @@
-evalNowcast <- function(doa_first = NULL, doa_last = Sys.Date(), d_max = 40, base = "Meldedatum", 
-                        n = 1000, save = T) {
-  library(tidyverse)
-  library(tidyr)
-  library(magrittr)
+evalNowcast <- function(doa_first = NULL, doa_last = Sys.Date(), base = "Meldedatum", 
+                        location_RKI = "DE", correction = T) {
+  
   library(lubridate)
-  library(mgcv)
-  library(readxl)
-  library(rgdal)
-  library(checkmate)
-  library(ggplot2)
-  library(readr)
   library(dplyr)
   
-  source("Nowcast_Hosp/02_Code/Functions/Preprocessing.R")
-  source("Nowcast_Hosp/02_Code/Functions/Fitting.R")
-
-  if (is.null(doa_first)) {
-    filenames <- unlist(list.files("Nowcast_Hosp/03_Results/Evaluation", 
-                                   pattern = "*.csv", 
-                                   full.names = TRUE))
-    filename_dates <- as.Date(substr(filenames, nchar(filenames) - 13, nchar(filenames) - 4))
-    
-    doa_first <- max(filename_dates) + days(1)
-    
-    old_data <- read.csv2(filenames[[which.max(filename_dates)]])
-    old_data$X <- NULL
-    old_data$date <- as.Date(old_data$date)
-  }
+  cov_corr <- ifelse(correction, "coverage_correction_", "")
+  
+  # if (is.null(doa_first)) {
+  #   filenames <- unlist(list.files("Nowcast_Hosp/03_Results/Evaluation",
+  #                                  pattern = "*.csv",
+  #                                  full.names = TRUE))
+  #   filename_dates <- as.Date(substr(filenames, nchar(filenames) - 13, nchar(filenames) - 4))
+  # 
+  #   doa_first <- max(filename_dates) + days(1)
+  # 
+  #   old_data <- read.csv2(filenames[[which.max(filename_dates)]])
+  #   old_data$X <- NULL
+  #   old_data$date <- as.Date(old_data$date)
+  # }
   
   # Beobachtung
   days <- seq(from = doa_first, to = doa_last - d_max, by = 1)
-  now_list <- list()
+  now_list_recent <- list()
   
   # Evaluation & Datenaufbereitung: Nowcasting für alle 8-wöchigen Zeitabschnitte
   for (d in seq_along(days)) {
-    now_list[[d]] <- nowcasting(T_0 = days[[d]] - days(56), doa = days[[d]], d_max = d_max, 
-                                base = base, save = save, n = n)
+    now_dat <- read.csv2(paste0("Nowcast_Hosp/03_Results/RKI_results/", days[[d]],
+                                "/", cov_corr, "nowcasting_results_", location_RKI, 
+                                "_", days[[d]], ".csv"))
+    
+    now_list_recent[[d]] <- now_dat %>%
+      filter(date == days[[d]]) %>%
+      select(c("date", "age60", "nowcast_est", "nowcast_0.025", "nowcast_0.975", 
+               "nowcast7_est", "nowcast7_0.025", "nowcast7_0.975"))
   }
-
-  # letzten Tag des Nowcasts
-  now_list_recent <- lapply(X = now_list, function(dat) {
-    dat[1:3, ]
-  })
   
   # Datensätze erstellen
   eval_data <- data.frame(do.call(rbind.data.frame, now_list_recent))
-
-  if (exists("old_data")) {
-    eval_data <- rbind(old_data, eval_data)
-  }
+  
+  # if (exists("old_data")) {
+  #   eval_data <- rbind(old_data, eval_data)
+  # }
+  
+  # new data
+  data_report <- read.csv2(paste0("Nowcast_Hosp/03_Results/RKI_results/", doa_last,
+                                  "/", cov_corr, "nowcasting_results_", location_RKI, 
+                                  "_", doa_last, ".csv")) %>%
+    select(c("date", "age60", "reported", "reported7"))
+  
+  # merge data
+  eval_data <- merge(eval_data, data_report, by = c("date", "age60"))
+  
   eval_data <- eval_data %>%
     mutate(date = as.Date(date), age60 = as.factor(age60))
-
-  filename <- paste0("Nowcast_Hosp/03_Results/Evaluation/eval_data_", 
-                     base, "_",
-                     min(eval_data$date) + days(1), "_bis_", doa_last - d_max,
-                     ".csv")
+  
+  filename <- paste0("Nowcast_Hosp/03_Results/Evaluation/",  cov_corr, "eval_data_", location_RKI, 
+                     min(eval_data$date) + days(1), "_to_", doa_last - d_max, ".csv")
   
   write.csv2(x = eval_data, file = filename)
   return(eval_data)
 }
 
 
-evalPlot <- function (data_eval, daily = F, age60 = "alle", base = "Meldedatum", doa = Sys.Date()) {
+evalPlot <- function (data_eval, daily = F, base = "Meldedatum", age = "alle", 
+                      location_RKI = "DE", correction = T) {
+  
   library(ggplot2)
-
-  if (age60 == "alle") {
-    age <- NULL
-  } else {
-    age <- ifelse(age60 == "ja", "60plus", "u60")
-  }
+  library(dplyr)
   
-  if (base == "Hospdatum") {
-    base <- "Hospitalisierungsdatum"
-  }
+  cov_corr <- ifelse(correction, "coverage_correction_", "")
+  base <- ifelse(base == "Hospdatum", "Hospitalisierungsdatum", base)
   
-  data_report <- read_csv2(paste0("Nowcast_Hosp/03_Results/nowcasting_results_", 
-                                   base, "_", doa, ".csv"))
+  data <- data_eval %>% filter(age60 == age)
   
-  if(daily) {
+  if (daily) {
     # täglicher Vergleich
-    plot <- ggplot() +
-      geom_line(aes(date, nowcast_est, col = "Nowcast"),
-                data = data_eval[data_eval$age60 == age60, ]) +
-      geom_line(aes(date, reported, col = "Meldungen"),
-                data = data_report[data_report$age60 == age60 & 
-                                     data_report$date %in% data_eval$date, ]) +
-      geom_ribbon(aes(date, ymin = nowcast_lwr, ymax = nowcast_upr),
-                  data = data_eval[data_eval$age60 == age60, ],
+    plot <- ggplot(data = data) +
+      geom_line(aes(date, nowcast_est, col = "Nowcast")) +
+      geom_line(aes(date, reported, col = "Meldungen")) +
+      geom_ribbon(aes(date, ymin = nowcast_0.025, ymax = nowcast_0.975),
                   fill = "cornflowerblue",
                   col = NA,
                   alpha = .4) +
@@ -95,27 +86,24 @@ evalPlot <- function (data_eval, daily = F, age60 = "alle", base = "Meldedatum",
       labs(x = base, 
            y ="Hospitalisierungen") +
       scale_x_date(date_labels = "%d.%m.%y") +
-      theme(axis.text=element_text(size=14), axis.title = element_text(size=14),
+      theme(axis.text = element_text(size = 14), 
+            axis.title = element_text(size = 14),
             legend.position = "top") +
       scale_color_manual(name = NULL, values = c("Nowcast" = "cornflowerblue",
                                                  "Meldungen" = "black"))
     
     filename <- paste0("Nowcast_Hosp/03_Results/Evaluation/Plots/eval_plot_daily", 
-                       age,
+                       age60,
                        "_",
                        tolower(substr(base, 1, 4)),
                        ".png")
     
   } else {
     # 7-tägiger Vergleich
-    plot <- ggplot() +
-      geom_line(aes(date, nowcast7_est, col = "Nowcast"), 
-                data = data_eval[data_eval$age60 == age60, ]) +
-      geom_line(aes(date, reported7, col = "Meldungen"),
-                data = data_report[data_report$age60 == age60 & 
-                                     data_report$date %in% data_eval$date, ]) +
-      geom_ribbon(aes(date, ymin = nowcast7_lwr, ymax = nowcast7_upr), 
-                  data = data_eval[data_eval$age60 == age60, ], 
+    plot <- ggplot(data = data) +
+      geom_line(aes(date, nowcast7_est, col = "Nowcast")) +
+      geom_line(aes(date, reported7, col = "Meldungen")) +
+      geom_ribbon(aes(date, ymin = nowcast7_0.025, ymax = nowcast7_0.975), 
                   fill = "cornflowerblue", 
                   col = NA,
                   alpha = .4) +
@@ -123,17 +111,19 @@ evalPlot <- function (data_eval, daily = F, age60 = "alle", base = "Meldedatum",
       labs(x = base, 
            y ="7-tägige Hospitalisierungen") +
       scale_x_date(date_labels = "%d.%m.%y", date_breaks = "15 days") +
-      theme(axis.text=element_text(size=14), axis.title = element_text(size=14), 
+      theme(axis.text = element_text(size = 14), 
+            axis.title = element_text(size = 14), 
             legend.position = "top") +
       scale_color_manual(name = NULL, values = c("Nowcast" = "cornflowerblue", 
                                                  "Meldungen" = "black"))
     
     filename <- paste0("Nowcast_Hosp/03_Results/Evaluation/Plots/eval_plot_7daily", 
-                       age,
+                       age60,
                        "_",
                        tolower(substr(base, 1, 4)),
                        ".png")
   }
+  
   ggsave(filename = filename, plot = plot)
   return(plot)
 }
