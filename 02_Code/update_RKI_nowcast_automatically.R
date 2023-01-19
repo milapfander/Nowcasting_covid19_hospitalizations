93001f9e77cc3e54e1569b9e3dd8db54fed9d070
ri87nix <maximilian.weigert@stat.uni-muenchen.de>
2023-01-19 18:36:19 +0100

    git lfs and setup for automatic procedure

--- 02_Code/update_RKI_nowcast_automatically.R
+++ 02_Code/update_RKI_nowcast_automatically.R
@@ -10,7 +10,7 @@ current_time <- now("CET")
 print(current_time)
 hour <- as.numeric(strftime(x = current_time, format = "%H"))
 day <- as_date(now())
-day <- if_else(file.exists(paste0("Nowcast_Hosp/03_Results/RKI_results/", day,
+day <- if_else(file.exists(paste0("03_Results/RKI_results/", day,
                                   "/", day, "-LMU_StaBLab-GAM_nowcast.csv")),
                day + days(1), day)
 
@@ -35,11 +35,11 @@ while(TRUE) {
   new_data <- FALSE
   while (new_data == FALSE) {
     download.file(url = "https://raw.githubusercontent.com/KITmetricslab/hospitalization-nowcast-hub/main/data-truth/COVID-19/COVID-19_hospitalizations_preprocessed.csv",
-                  destfile = paste0("Nowcast_Hosp/01_Data/Data_RKI/COVID-19_hospitalizations_preprocessed_",
+                  destfile = paste0("01_Data/Data_RKI/COVID-19_hospitalizations_preprocessed_",
                                     day, ".csv"))
-    data_new <- data.table(read.csv(paste0("Nowcast_Hosp/01_Data/Data_RKI/COVID-19_hospitalizations_preprocessed_",
+    data_new <- data.table(read.csv(paste0("01_Data/Data_RKI/COVID-19_hospitalizations_preprocessed_",
                                            day, ".csv")))
-    data_old <- data.table(read.csv(paste0("Nowcast_Hosp/01_Data/Data_RKI/COVID-19_hospitalizations_preprocessed_",
+    data_old <- data.table(read.csv(paste0("01_Data/Data_RKI/COVID-19_hospitalizations_preprocessed_",
                                            day - 1, ".csv")))
     if (data_new$date[nrow(data_new)] == data_old$date[nrow(data_old)]) {
       print("no new data available")
@@ -52,20 +52,20 @@ while(TRUE) {
   }
   
   # Run analysis script:
-  source("Nowcast_Hosp/02_Code/Analysis_RKI_states.R")
+  source("02_Code/Analysis_RKI_states.R")
   paste("Update finished at", now("CET"))
   
   # Automize Git process:
   # Commit results to local repository:
-  cred_gitlab <- cred_token(token = "GITLAB_PAT")
-  git2r::pull(repo = getwd(), credentials = cred_gitlab)
+  cred_github <- cred_token(token = "GITHUB_PAT")
+  git2r::pull(repo = getwd(), credentials = cred_github)
   git2r::add(repo = getwd(), path = file.path(getwd(), "/*"))
   git2r::commit(repo = getwd(),
          message = paste0("nowcasting results LMU_StaBLab_", as_date(now())))
-  git2r::push(object = getwd(), credentials = cred_gitlab)
+  git2r::push(object = getwd(), credentials = cred_github)
   
   # Copy file to forked GitHub repository:
-  file.copy(from = paste0("Nowcast_Hosp/03_Results/RKI_results/",
+  file.copy(from = paste0("03_Results/RKI_results/",
                           as_date(now()), "/", as_date(now()),
                           "-LMU_StaBLab-GAM_nowcast.csv"),
             to = paste0("../hospitalization-nowcast-hub/data-processed/LMU_StaBLab-GAM_nowcast/",
@@ -73,7 +73,6 @@ while(TRUE) {
   
   # Commit results to forked GitHub repository:
   nowcast_repo <- paste0("../hospitalization-nowcast-hub")
-  cred_github <- cred_token(token = "GITHUB_PAT")
   git2r::pull(repo = nowcast_repo, credentials = cred_github)
   #git2r::fetch(repo = nowcast_repo, name = "upstream") # commented out due to possible merge conflicts with automatic pull request
   #git2r::merge(x = nowcast_repo, y = "upstream/main")
@@ -82,84 +81,6 @@ while(TRUE) {
   git2r::commit(repo = nowcast_repo,
          message = paste0("nowcasting results LMU_StaBLab_", as_date(now())))
   git2r::push(object = nowcast_repo, credentials = cred_github)
-  
-  # Create pull request:
-  # TO DO
-  # not sure if that works directly from R
-  
-  # Run changepoint analysis on wednesdays:
-  if (weekdays(day) == "Wednesday") {
-    # wait 8 hours
-    Sys.sleep(28800)
-    tryCatch({ # hosp
-        # create dataset for hosp cp
-        source("Nowcast_Hosp/02_Code/Functions/formatting_nowcast_data.R")
-        
-        setwd("../changepoint_analysis/")
-        
-        # run cp analysis
-        doa <- day
-        ## hosp
-        data_path <- "../inzicuhosp/Nowcast_Hosp/03_Results/RKI_results/bruchpunkt_nowcast_abs.rds"
-        # run hosp analysis
-        source("hospitalization/code/hosp_analysis.R")
-        # create result files
-        source("hospitalization/code/hosp_plots.R")
-        
-        setwd("../inzicuhosp/")
-        
-      },
-      error = function(e) print(e))
-    
-    if(str_detect(getwd(), "changepoint_analysis"))
-      setwd("../inzicuhosp/")
-    
-    tryCatch({ # icu
-        setwd("../changepoint_analysis/")
-        
-        # manually put data in the icu/data folder
-        # download does not work since the download url changes with the current browser session
-        data_path <- paste0("icu/data/divi_admissions.rds")
-        # scrape icu admissions
-        source("icu/code/scrape_icu.R")
-        # run icu analysis
-        source("icu/code/icu_analysis.R")
-        # create result files
-        source("icu/code/icu_plots.R")
-        
-        setwd("../inzicuhosp/")
-      },
-      error = function(e) print(e))
-    
-    # Automize Git process:
-    tryCatch({ # icu
-      # Commit results to forked GitHub repository:
-      setwd("../changepoint_analysis/")
-      
-      cred_gitlab <- cred_token(token = "GITLAB_PAT")
-      git2r::pull(repo = getwd(), credentials = cred_gitlab)
-      git2r::add(repo = getwd(), path = file.path(getwd(), "/*"))
-      git2r::commit(repo = getwd(),
-                    message = paste0("results cp analysis ", as_date(now())))
-      git2r::push(object = getwd(), credentials = cred_gitlab)
-      
-      setwd("../inzicuhosp/")
-      
-      # nowcast_repo <- paste0("../changepoint_analysis")
-      # cred_gitlab <- cred_token(token = "GITLAB_PAT")
-      # git2r::pull(repo = nowcast_repo, credentials = cred_gitlab)
-      # 
-      # git2r::add(repo = nowcast_repo,
-      #            path = file.path("/home/ubuntu/changepoint_analysis/*"))
-      # git2r::commit(repo = nowcast_repo,
-      #               message = paste0("results cp analysis ", as_date(now())))
-      # git2r::push(object = nowcast_repo, credentials = cred_gitlab)
-    },
-    error = function(e) print(e))
-    
-    if(str_detect(getwd(), "changepoint_analysis"))
-      setwd("../inzicuhosp/")
-  }
   
   # Incrase day for execution time by one unit:
   if (now("CET") > exec_time) {
