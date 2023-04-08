9c039367958d97b77ebe1fbbc630f91a73679f22
ri87nix <maximilian.weigert@stat.uni-muenchen.de>
2023-04-08 08:34:47 +0200

    nowcasting results LMU_StaBLab_2023-04-07

--- 02_Code/Functions/Intervals.R
+++ 02_Code/Functions/Intervals.R
@@ -63,11 +63,11 @@ interval_correction <- function(doa, location, interval_vec = c(0.8, 0.95),
   # read data from 3 to 1.5 months ago
   data_list <- lapply(X = dates, 
                       FUN = function(x) {
-                        file <- paste0("Nowcast_Hosp/03_Results/RKI_results", 
+                        file <- paste0("03_Results/RKI_results", 
                                        retro, x, "/nowcasting_results_",
                                        location, "_", x, ".csv")
                         if (file.exists(file)) {
-                          read_csv2(paste0("Nowcast_Hosp/03_Results/RKI_results", 
+                          read_csv2(paste0("03_Results/RKI_results", 
                                            retro, x, "/nowcasting_results_",
                                            location, "_", x, ".csv")) %>%
                             mutate(horizon = as.numeric(x - date)) %>%
